df50bbc8ec3faff629f84ba3383b8792fcb64881
armixas <arminas448@gmail.com>
2023-03-21 10:41:29 +0200

    Scene update and collectibles tags

---
+++ Assets/Scripts/Collectibles/HoverAndRotate.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverAndRotate : MonoBehaviour
+{
+    [SerializeField] private float hoverDist = 0.25f;
+    [SerializeField] private float hoverSpeed = 1f;
+    void Update()
+    {
+        Vector3 hoverpos = new Vector3(
+            transform.position.x,
+            transform.position.y + Mathf.Sin(Time.time * hoverSpeed) * hoverDist,
+            transform.position.z);
+        transform.Rotate(new Vector3(0, 50, 0) * Time.deltaTime, Space.Self);
+        transform.Rotate(new Vector3(0, 70, 0) * Time.deltaTime, Space.World);
+    }
+}
