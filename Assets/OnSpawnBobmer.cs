3d362fdfe7f19b293234ed5d29255ef728afbb65
Egle <103460996+IdiotAllex@users.noreply.github.com>
2023-05-30 13:54:08 +0300

    tweaks

--- Assets/OnSpawnBobmer.cs
+++ Assets/OnSpawnBobmer.cs
@@ -18,51 +18,51 @@ public class OnSpawnBobmer : MonoBehaviour
     private PlayerInput playerInput;
     private GameObject healthGameObject;
 
-    //private void Awake()
-    //{
-    //    if (GetComponent<GridMovement>().enabled == false)
-    //    {
-    //        GetComponent<GridMovement>().enabled = true;
-    //    }
-    //    right = GameObject.Find("Ready");
-    //    left = GameObject.Find("Ready1");
-    //    Button leftButton = left.GetComponent<Button>();
+    private void Awake()
+    {
+        if (GetComponent<GridMovement>().enabled == false)
+        {
+            GetComponent<GridMovement>().enabled = true;
+        }
+        right = GameObject.Find("Ready");
+        left = GameObject.Find("Ready1");
+        Button leftButton = left.GetComponent<Button>();
 
-    //    Button rightButton = right.GetComponent<Button>();
-    //    playerInput = GetComponent<PlayerInput>();
-    //    InputActionMap actionMap = playerInput.currentActionMap;
+        Button rightButton = right.GetComponent<Button>();
+        playerInput = GetComponent<PlayerInput>();
+        InputActionMap actionMap = playerInput.currentActionMap;
 
-    //    if (string.Join("\n", actionMap.FindAction("MoveForward").controls).Contains("Left"))
-    //    {
+        if (string.Join("\n", actionMap.FindAction("MoveForward").controls).Contains("Left"))
+        {
 
 
-    //        //player1
-    //        int selectedCharacter = PlayerPrefs.GetInt("SelecetedCharacter" + 1);
-    //        GameObject model = Instantiate(models[selectedCharacter]);
-    //        model.transform.position = Vector3.zero;
-    //        model.transform.parent = transform;
-    //        transform.position = spawnPoint1.transform.position;
+            //player1
+            //int selectedCharacter = PlayerPrefs.GetInt("SelecetedCharacter" + 1);
+            //GameObject model = Instantiate(models[selectedCharacter]);
+            //model.transform.position = Vector3.zero;
+            //model.transform.parent = transform;
+            transform.position = spawnPoint1.transform.position;
 
-    //        leftButton.interactable = false;
+            leftButton.interactable = false;
 
-    //        healthGameObject = GameObject.Find("Player1");
-    //        playerNumber = 1;
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("Player2");
-    //        //player2
-    //        int selectedCharacter = PlayerPrefs.GetInt("SelecetedCharacter" + 2);
-    //        GameObject model = Instantiate(models[selectedCharacter]);
-    //        model.transform.position = Vector3.zero;
-    //        model.transform.parent = transform;
-    //        transform.position = spawnPoint2.transform.position;
+            healthGameObject = GameObject.Find("Player1");
+            playerNumber = 1;
+        }
+        else
+        {
+            Debug.Log("Player2");
+            //player2
+            //int selectedCharacter = PlayerPrefs.GetInt("SelecetedCharacter" + 2);
+            //GameObject model = Instantiate(models[selectedCharacter]);
+            //model.transform.position = Vector3.zero;
+            //model.transform.parent = transform;
+            transform.position = spawnPoint2.transform.position;
 
-    //        rightButton.interactable = false;
+            rightButton.interactable = false;
 
-    //        healthGameObject = GameObject.Find("Player2");
-    //        playerNumber = 2;
-    //    }
+            healthGameObject = GameObject.Find("Player2");
+            playerNumber = 2;
+        }
 
-    //}
+    }
 }
